09533ed879c8bff4855ecd56dced2c30f303241b
vlbandara <137267283+vlbandara@users.noreply.github.com>
2024-12-26 22:54:00 +0530

    Create WebSocket Configuration,Create a Controller for Signaling,created SignallingMessage model

--- src/main/java/com/aggrandizer/PeerJam/config/WebSocketConfig.java
+++ src/main/java/com/aggrandizer/PeerJam/config/WebSocketConfig.java
@@ -1,24 +1,25 @@
 package com.aggrandizer.PeerJam.config;
 
-import com.aggrandizer.PeerJam.handler.SignalingServerHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final SignalingServerHandler signalingHandler;
-
-    public WebSocketConfig(SignalingServerHandler signalingHandler) {
-        this.signalingHandler = signalingHandler;
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(signalingHandler, "/signaling-server").setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 }
