e1513514626c259a043c9b14f76bb5f955b7444a
vlbandara <137267283+vlbandara@users.noreply.github.com>
2025-01-14 22:44:30 +0530

    add chat functionality

--- src/main/java/com/aggrandizer/PeerJam/PeerJam.java
+++ src/main/java/com/aggrandizer/PeerJam/PeerJam.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeerJamApplication {
+public class PeerJam {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeerJamApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PeerJam.class, args);
+    }
+
 }
